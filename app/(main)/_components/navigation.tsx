1d640cc72f2c1ca64f387e2d0019b361d63d2a1d
Artorias <105161738+artorias305@users.noreply.github.com>
2023-11-14 15:01:11 -0300

    Settings

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -27,8 +27,10 @@ import {
 import { TrashBox } from "./trash-box";
 
 import { useSearch } from "@/hooks/use-search";
+import { useSettings } from "@/hooks/use-settings";
 
 export const Navigation = () => {
+  const settings = useSettings();
   const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -148,7 +150,7 @@ export const Navigation = () => {
         <div>
           <UserItem />
           <Item label="search" icon={Search} isSearch onClick={search.onOpen} />
-          <Item label="Settings" icon={Settings} onClick={() => {}} />
+          <Item label="Settings" icon={Settings} onClick={settings.onOpen} />
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
